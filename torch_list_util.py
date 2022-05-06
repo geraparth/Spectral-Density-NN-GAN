ce05ccb7fa0fdf7eba807856396829c09e54843b
Parth Gera <gera2@illinois.edu>
2022-05-05 19:07:47 -0500

    tensor_list_to_vector added

---
+++ torch_list_util.py
@@ -0,0 +1,18 @@
+import torch
+from typing import List
+
+
+def tensor_list_to_vector(tor_list: List[torch.tensor]) -> torch.tensor:
+    """
+    Used to convert a model's parameters, which are a list of torch variables
+    into a single vector (Input for Lanczos tridiagonalization)
+
+    Input:
+        Torch tensor list: List of torch tensors to convert into a single vector
+
+    Returns:
+        A torch tensor (1-D vector) created by concatening and reshaping the original list of tensors
+        The shape of the vector is [w_dim, 1] where w_dim is the sum of the number of scalars in the list of tensors
+    """
+
+    return torch.cat([x.view(-1, 1) for x in tor_list], axis=0)
