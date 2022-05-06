620c64df0a6c4c926be3ab2db3083ae16f5a0c99
Parth Gera <gera2@illinois.edu>
2022-05-05 19:13:16 -0500

    Requirements added

--- torch_list_util.py
+++ torch_list_util.py
@@ -14,5 +14,4 @@ def tensor_list_to_vector(tor_list: List[torch.tensor]) -> torch.tensor:
         A torch tensor (1-D vector) created by concatening and reshaping the original list of tensors
         The shape of the vector is [w_dim, 1] where w_dim is the sum of the number of scalars in the list of tensors
     """
-
     return torch.cat([x.view(-1, 1) for x in tor_list], axis=0)
